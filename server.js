5ba2580d68973f0d40874c9c7986423b3326072e
Amit Bourmad <amitb@securenative.com>
2019-05-21 22:35:36 +0300

    Socket controller as seperate file

--- server.js
+++ server.js
@@ -2,40 +2,18 @@ const server = require('http').createServer();
 const io = require('socket.io')(server);
 const events = require('./socket/SocketEvents');
 const SocketManager = require('./socket/SocketManager');
-
 const socketManager = SocketManager.getInstance();
+const SocketController = require('./socket/SocketController');
 
 io.on('connection', socket => {
 
-    socket.on(events.USER_CONNECT, ({ userId }) => {
-        // TODO: User connects save to socket manager
-        socketManager.cacheUser(userId, socket);
-    })
+    const controller = SocketController(socketManager, socket);
 
-    socket.on(events.HARDWARE_CONNECT, data => {
-        // TODO: Hardware connects save to socket manager
-        socketManager.cacheHardware(socket);
-    })
-
-    socket.on(events.SENSOR_DATA, data => {
-        console.log('Data arrived', data);
-        socketManager.emitUsers('clientData', data);
-        // sensorData = data;
-    });
-
-    socket.on(events.TOGGLE_RELAY, data => {
-        socketManager.emitHardware(events.TOGGLE_RELAY, data);
-    })
-    
-    socket.on('disconnect', () => { /* … */ });
-    
-    // TODO: Create data structure that contains omega client, and browser clients array
-    // TODO: Remove from here
-    // setInterval(() => {
-    //     if (sensorData) {
-    //         io.emit('clientData', sensorData);
-    //     }
-    // }, 2000);
+    socket.on(events.USER_CONNECT, controller.handleUserConnect)
+    socket.on(events.HARDWARE_CONNECT, controller.handleHardwareConnect)
+    socket.on(events.SENSOR_DATA, controller.handleSensorData);
+    socket.on(events.TOGGLE_RELAY, controller.handleToggleRelay)
+    socket.on('disconnect', controller.handleDisconnect);
 });
 
 server.listen(3001);
